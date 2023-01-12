ff9aa53d24da3eea0fa4e77b9b12558c4e2a4d6b
jtranberg <35013712+jtranberg@users.noreply.github.com>
2023-01-12 13:25:21 -0800

    commit with fixes, percip still unavailable in hourly

--- main.js
+++ main.js
@@ -14,7 +14,7 @@ function positionSuccess({ coords }) {
 (renderWeather)
 .catch(e => {
   console.error(e)
-  // alert("error getting weather.")
+  alert("error getting weather.")
 })
 }
 
@@ -27,11 +27,11 @@ alert("error getting your position. Please allow us to use your location and ref
 function renderWeather ({ current, daily, hourly}) {
   renderCurrentWeather(current)
   renderDailyWeather(daily)
-  // renderHourlyWeather(hourly)
+  renderHourlyWeather(hourly)
   document.body.classList.remove("blurred")
 }
 
-function setValue(selector, value, {parent =document} = {}) {
+function setValue(selector, value, {parent = document} = {}) {
   parent.querySelector(`[data-${selector}]`) .textContent = value
 }
 
@@ -43,12 +43,12 @@ const currentIcon = document.querySelector("[data-current-icon]")
 function renderCurrentWeather(current) {
   currentIcon.src = getIconUrl(current.iconCode)
   setValue("current-temp", current.currentTemp )
-  setValue("current-high", current.higTemp )
+  setValue("current-high", current.highTemp )
   setValue("current-low", current.lowTemp )
   setValue("current-fl-high", current.highFeelsLike )
   setValue("current-fl-low", current.lowFeelsLike)
   setValue("current-wind", current.windSpeed )
-  setValue("current-precip", current.percip )
+  setValue("current-precip", current.precip )
   
 }  
 
@@ -60,7 +60,7 @@ function renderDailyWeather(daily) {
    daily.forEach(day => {
     const element = dayCardTemplate.content.cloneNode(true)
     setValue("temp", day.maxTemp, {parent: element})
-    setValue("date", DAY_FORMATTER.format(day.timestampt), {parent: element})
+    setValue("date", DAY_FORMATTER.format(day.timestamp), {parent: element})
     element.querySelector("[data-icon]").src = getIconUrl(day.iconCode) 
     dailySection.append(element)
   })
@@ -70,17 +70,17 @@ const HOUR_FORMATTER = new Intl.DateTimeFormat(undefined, {hour: "numeric"})
 const hourlySection = document.querySelector("[data-hour-section]")
 const hourRowTemplate = document.getElementById("hour-row-template")
 
-// function renderHourlyWeather(hourly) {
-//   hourlySection.innerHTML = ""
-//   hourly.forEach(hour => {
-//    const element = hourRowTemplate.content.cloneNode(true)
-//    setValue("temp", hour.Temp, {parent: element})
-//    setValue("fl-temp", hour.feelsLike, {parent: element})
-//    setValue("wind", hour.windSpeed, {parent: element})
-//    setValue("precip", hour.percip, {parent: element})
-//    setValue("day", DAY_FORMATTER.format() (hour.timestampt), {parent: element})
-//    setValue("time", HOUR_FORMATTER.format() (hour.timestampt), {parent: element})
-//    element.querySelector("[data-icon]").src = getIconUrl(hour.iconCode) 
-//    hourlySection.append(element)
-//   })
-// }
+function renderHourlyWeather(hourly) {
+  hourlySection.innerHTML = ""
+  hourly.forEach(hour => {
+   const element = hourRowTemplate.content.cloneNode(true)
+   setValue("temp", hour.temp, {parent: element})
+   setValue("fl-temp", hour.feelsLike, {parent: element})
+   setValue("wind", hour.windSpeed, {parent: element})
+  //  setValue("precip", hour.precip, {parent: element})
+   setValue("day", DAY_FORMATTER.format(hour.timestamp), {parent: element})
+   setValue("time", HOUR_FORMATTER.format(hour.timestamp), {parent: element})
+   element.querySelector("[data-icon]").src = getIconUrl(hour.iconCode) 
+   hourlySection.append(element)
+  })
+}
