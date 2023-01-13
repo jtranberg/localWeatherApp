244b3aac9bc481d038e935c4bb9c244b066214d4
jtranberg <35013712+jtranberg@users.noreply.github.com>
2023-01-12 19:31:44 -0800

    metric conversion

--- weather.js
+++ weather.js
@@ -1,11 +1,11 @@
 import axios from "axios"
 
-
+//https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=temperature_2m,apparent_temperature,precipitation,weathercode,windspeed_10m&daily=weathercode,temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min,precipitation_sum&current_weather=true&timeformat=unixtime&timezone=America%2FLos_Angeles
 //https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=temperature_2m,apparent_temperature,precipitation,weathercode,windspeed_10m&daily=weathercode,temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min,precipitation_sum&current_weather=true&timeformat=unixtime&timezone=America%2FLos_Angeles
 
 export  function getWeather(lat, lon, timezone) {
  return axios.get(
-    "https://api.open-meteo.com/v1/forecast?latitude&longitude&hourly=temperature_2m,apparent_temperature,precipitation,weathercode,windspeed_10m&daily=weathercode,temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min,precipitation_sum&current_weather=true&temperature_unit=celsius&windspeed_unit=mph&precipitation_unit=inch&timeformat=unixtime",
+    "https://api.open-meteo.com/v1/forecast?latitude&longitude&hourly=temperature_2m,apparent_temperature,precipitation,weathercode,windspeed_10m&daily=weathercode,temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min,precipitation_sum&current_weather=true&temperature_unit=celsius&windspeed_unit=kmh&precipitation_unit=mm&timeformat=unixtime",
     {
       params: {
         latitude: lat,
